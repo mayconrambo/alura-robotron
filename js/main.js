8a051da7b4efcfacd06bead09c174c7056190a32
Maycon Willian Rambo <maycon.rambo11@gmail.com>
2023-01-10 12:55:05 -0300

    alter fontes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,58 @@
-const robotron = document.querySelector("#robotron");
+const controle 	= document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
 
-robotron.addEventListener("click", DizOi);
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-function DizOi() {
-    console.log("Oi");
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
+controle.forEach( (elemento) => {
+	elemento.addEventListener("click", (event) => {
+		manipulandoDados(event.target.dataset.controle, event.target.parentNode);
+		atualizaEstatisticas(event.target.dataset.peca);
+	});
+});
+
+function manipulandoDados(Operacao, Controle){
+	const peca 	= Controle.querySelector("[data-contador]");
+	if(Operacao === "-"){
+		peca.value = parseInt(peca.value) - 1;
+	} else {
+		peca.value = parseInt(peca.value) + 1;
+	}
+}
+
+function atualizaEstatisticas(peca){
+	estatisticas.forEach( (elemento) => {
+		elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]	
+	});
 }
